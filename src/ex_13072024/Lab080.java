5e117988a291915863f4139885d9123b4fad39e5
SumedTambe <sumed.tambe@gmail.com>
2025-12-21 10:41:58 +0530

    CoreJava

---
+++ src/ex_13072024/Lab080.java
@@ -0,0 +1,12 @@
+package ex_13072024;
+
+public class Lab080 {
+    public static void main(String[] args) {
+        // Max between two numbers.
+        int a = 10;
+        int b = 20;
+
+        int c = a > b ? 10 : 20;
+        System.out.println(c);
+    }
+}
---
+++ src/ex_13072024/Lab081.java
@@ -0,0 +1,12 @@
+package ex_13072024;
+import java.lang.Math;
+
+public class Lab081 {
+    public static void main(String[] args) {
+        double sq = Math.sqrt(25);
+        System.out.println(sq);
+
+        int result = Math.max(10,20);
+        System.out.println(result);
+    }
+}

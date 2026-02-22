e452aed063abbaf48314b436381927a332d9f156
SumedTambe <sumed.tambe@gmail.com>
2026-02-21 23:39:25 -0700

    02212026

---
+++ src/ex_02202026/P_108.java
@@ -0,0 +1,12 @@
+package ex_02202026;
+
+public class P_108 {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Java");
+        sb.append(" Programming");
+        sb.append(" Language");
+
+        System.out.println(sb);
+    }
+}
---
+++ src/ex_02212026/P_110.java
@@ -0,0 +1,31 @@
+package ex_02212026;
+
+public class P_110 {
+    public static void main(String[] args) {
+        //Example 1: Car 🚗
+        Car car1 = new Car(); //Object
+        car1.brand = "BMW";
+        car1.color = "Black";
+        car1.speed = 0;
+
+        car1.start();  //Behavior
+        car1.accelerate(); //Behavior
+    }
+}
+
+class Car{
+    //Attribute (Data)
+    String brand;
+    String color;
+    int speed;
+
+    //Behavior
+    void start(){
+        System.out.println(brand+" is starting...");
+    }
+
+    void accelerate(){
+        speed += 20;
+        System.out.println("Speed is now: "+speed);
+    }
+}
